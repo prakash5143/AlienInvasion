7469fbdfccdc198176c30b6d67d7c9075b861442
Messina <patrickmessina@gmail.com>
2016-05-19 00:33:40 -0500

    Finished from book.
    Finished what was in the book. Plan on adding more material.

--- ship.py
+++ ship.py
@@ -1,11 +1,13 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
 
   def __init__(self, ai_settings,  screen):
     """
     Initialize the ship and set its starting position.
     """
+    super(Ship, self).__init__()
     self.screen = screen
     self.ai_settings = ai_settings
 
@@ -44,3 +46,9 @@ class Ship():
     Draw the ship at it's current location.
     """
     self.screen.blit(self.image, self.rect)
+
+  def center_ship(self):
+    """
+    Center the ship on the screen.
+    """
+    self.center = self.screen_rect.centerx
